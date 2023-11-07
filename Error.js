837d716e00528097d1cce5620fabef164a9d193e
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2023-11-07 20:02:54 +0100

    Add files via upload

---
+++ Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div className="container">
+      <h1>Error: Las credenciales son incorrectas</h1>
+    </div>
+  );
+}
+
+export default Error;
