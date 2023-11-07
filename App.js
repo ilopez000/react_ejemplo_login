9bc2313ddbe414c775a37a7087c3ef6bb1d0cbfb
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2023-11-07 20:01:50 +0100

    Update App.js

--- App.js
+++ App.js
@@ -3,24 +3,31 @@ import './App.css';
 import React, { useState } from 'react';
 import Login from './Login';
 import Home from './Home';
+import Error from './Error';
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loginAttemptFailed, setLoginAttemptFailed] = useState(false);
 
-  const handleLogin = (success) => {
-    setIsAuthenticated(success);
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+    setLoginAttemptFailed(false);
+  };
+
+  const handleLoginFailure = () => {
+    setLoginAttemptFailed(true);
   };
 
   return (
     <div>
-      {!isAuthenticated ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <Home />
+      {!isAuthenticated && !loginAttemptFailed && (
+        <Login onLogin={handleLoginSuccess} onError={handleLoginFailure} />
       )}
+      {isAuthenticated && <Home />}
+      {loginAttemptFailed && <Error />}
     </div>
   );
 }
 
 export default App;
-
